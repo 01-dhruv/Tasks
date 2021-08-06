89a9d0bb454397427e7a53d4ad1ba963a3776038
01-dhruv <Dhruvgandhi0712@gmail.com>
2021-08-06 22:55:43 +0530

    Poke api

--- Pokedex/views.py
+++ Pokedex/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 import requests
 
 
+# Create your views here.
+
+
 def index(request):
     data = True
     result = None
@@ -15,11 +18,24 @@ def index(request):
             result = requests.get('https://pokeapi.co/api/v2/type/')
             json = result.json()
             poke_type = (json['results'])
+            r1 = requests.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+            ab = (r1.json()['results'])
+
+
+            # Pokemon type Display
+            r2 = requests.get('https://pokeapi.co/api/v2/pokemon/1')
+            cd = (r2.json()['types'])
+
+
+            # for x in range(1, 152):
+            #     r2 = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(x))
+            #     cd = (r2.json()['types'])
+            #     pass
+            # print(r2.json()['types'])
 
 
 
             data = False
         except:
             data = True
-
     return render(request, 'index.html', {'poke_type': poke_type, 'ab': ab, 'cd': cd,})
